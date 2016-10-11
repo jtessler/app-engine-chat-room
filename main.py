91d3232bb6f3f4b1d4ecb53563132e2010ad3447
Joe Tessler <jrt@google.com>
2016-10-10 22:33:58 -0400

    Switch to inbound services
    Rather than waiting for the client to send another request notifying
    it's ready, just use the inbound service endpoints:
    
      - /_ah/channel/connected/
      - /_ah/channel/disconnected/
    
    Much more reliable.

--- main.py
+++ main.py
@@ -11,48 +11,52 @@ class ChatUser(ndb.Model):
   """ Stored user model. Should be keyed by the User ID. """
   nickname = ndb.StringProperty()
 
+class HandleConnect(webapp2.RequestHandler):
+  """ Send a welcome message and notifies all other users. """
+  def post(self):
+    user_id = self.request.get('from')
+    chat_user = ChatUser.get_by_id(user_id)
+
+    existing_users = ChatUser.query(ChatUser.key != chat_user.key);
+    channel.send_message(chat_user.key.string_id(),
+                         'Welcome, %s! Other chatters: %d' %
+                         (chat_user.nickname, existing_users.count()))
+    for existing_user in existing_users.iter():
+      channel.send_message(existing_user.key.string_id(),
+                           '%s joined' % chat_user.nickname)
+
+class HandleDisconnect(webapp2.RequestHandler):
+  """ Deletes the user model and notifies all other users. """
+  def post(self):
+    user_id = self.request.get('from')
+    chat_user = ChatUser.get_by_id(user_id)
+    chat_user.key.delete()
+
+    remaining_users = ChatUser.query()
+    for user in remaining_users.iter():
+      channel.send_message(user.key.string_id(), '%s left' % chat_user.nickname)
+
 class HandleSend(webapp2.RequestHandler):
   """ When a user sends a message to be echoed to all other users. """
   def post(self):
+    user_id = users.get_current_user().user_id()
+    chat_user = ChatUser.get_by_id(user_id)
+
     data = self.request.get('data')
-    users = ChatUser.query();
-    for user in users.iter():
-      channel.send_message(user.key.string_id(),
-                           '%s: %s' % (user.nickname, cgi.escape(data)))
-
-class HandleJoin(webapp2.RequestHandler):
-  """ Stores a new user model and notifies all other users. """
-  def post(self):
-    user = users.get_current_user()
-    new_chat_user = ChatUser.get_or_insert(user.user_id(),
-                                           nickname = user.nickname())
-
-    existing_users = ChatUser.query(ChatUser.key != new_chat_user.key);
-    channel.send_message(new_chat_user.key.string_id(),
-                         'Welcome, %s! Other chatters: %d' %
-                         (new_chat_user.nickname, existing_users.count()))
-    for existing_user in existing_users.iter():
-      channel.send_message(existing_user.key.string_id(),
-                           '%s joined' % new_chat_user.nickname)
-
-class HandleLeave(webapp2.RequestHandler):
-  """ Deletes the user model and notifies all other users. """
-  def post(self):
-    user = users.get_current_user()
-    chat_user = ChatUser.get_by_id(user.user_id())
-    chat_user.key.delete()
-
-    other_users = ChatUser.query();
-    for user in other_users.iter():
-      channel.send_message(user.key.string_id(),
-                           '%s left' % user.nickname)
+    recipients = ChatUser.query();
+    for recipient in recipients.iter():
+      channel.send_message(recipient.key.string_id(),
+                           '%s: %s' % (chat_user.nickname, cgi.escape(data)))
 
 class HandleMain(webapp2.RequestHandler):
   """ Renders index.html an initializes the chat room channel. """
   @login_required
   def get(self):
     user = users.get_current_user()
-    token = channel.create_channel(user.user_id())
+    chat_user = ChatUser.get_or_insert(user.user_id(),
+                                       nickname = user.nickname())
+
+    token = channel.create_channel(chat_user.key.string_id())
     template = jinja_environment.get_template('index.html')
     self.response.out.write(template.render({ 'token': token }))
 
@@ -61,7 +65,7 @@ jinja_environment = jinja2.Environment(
 
 app = webapp2.WSGIApplication([
   ('/', HandleMain),
-  ('/join', HandleJoin),
   ('/send', HandleSend),
-  ('/leave', HandleLeave),
+  ('/_ah/channel/connected/', HandleConnect),
+  ('/_ah/channel/disconnected/', HandleDisconnect),
 ], debug=True)
